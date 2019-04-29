856a1c422ce6b9fb681b6561c91c9841686856d1
sepy97 <sepy97@gmail.com>
2019-04-29 22:27:49 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+#define MAX_THREAD_NUM 10
+
+class LockStack {
+public:
+	LockStack() :head(NULL) {}
+	
+	void push(int d)
+	{
+		Node *pv = new Node;
+		
+		m.lock ();
+		
+		pv->d = d;
+		pv->p = head;
+		head = pv;
+		
+		m.unlock ();
+	}
+	int pop()
+	{
+		if (head == NULL) return NULL;
+		/*if (isEmpty())
+		{
+   			return -1;
+		}*/
+		m.lock ();
+		
+		int temp = head->d;
+		Node *pv = head;
+		head = head->p;
+		
+		m.unlock ();
+		
+		delete pv;
+		return temp;
+	}
+	bool isEmpty();
+	void display();
+
+private:
+	struct Node {
+		int d;
+		Node *p;
+	};
+	
+	Node *head;
+	std::mutex m;
+};
+
+void testPush (LockStack* toTest, int data)
+{
+	toTest->push (data);
+}
+
+int main()
+{
+	LockStack toTest;
+	
+	std::thread thr[MAX_THREAD_NUM];
+	
+	for (int i = 0; i < MAX_THREAD_NUM; i++)
+	{
+		//toTest.push (i);
+		thr[i] = std::thread (testPush, &toTest, i);
+	}
+	
+	for (int i = 0; i < MAX_THREAD_NUM; i++)
+	{
+		thr[i].join ();
+	}
+	
+	for (int i = 0; i < MAX_THREAD_NUM; i++)
+	{
+		printf ("%d\n", toTest.pop());
+	}
+	
+	return 0;
+}
